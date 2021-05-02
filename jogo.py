ded727e4da705f85787811bacaefbf6f091ce8df
Mateus <mat.rggo@gmail.com>
2021-05-02 14:19:38 -0300

    Condição para o jogo acabar

--- jogo.py
+++ jogo.py
@@ -7,3 +7,18 @@ if inicio == 's':
 else:
     print('Não vai jogar')
     jogar = False
+
+while jogar:
+    final = funcoes.fim_do_jogo(baralho)
+    if final:
+        if baralho == []:
+            print('Alguma coisa deu errado')
+            jogar = False
+        elif len(baralho) == 1:
+            print('Parabéns você ganhou!')
+            jogar = False
+            break
+        else:
+            print('Você perdeu, melhor sorte na próxima.')
+            jogar = False
+            break
